691193e82feaccf7723e0d02b2ec93bdbfc196a0
Matt Mueller <mattmuelle@gmail.com>
2011-10-05 01:55:52 -0700

    Allow continuous development using source, just need to link npm package and then we don't have to cake watch anymore.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require('lib/cheerio.js');
+// Use source if we have coffeescript otherwise use lib
+try {
+    require('coffee-script');
+    module.exports = require(__dirname + "/src/cheerio.coffee");
+} catch (e) {
+    module.exports = require(__dirname + "/lib/cheerio.js");
+}
