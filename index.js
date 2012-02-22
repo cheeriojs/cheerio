16d634e1b8caefe53d7e8905c6470f85567b6ffd
Siddharth Mahendraker <siddharth_mahen@me.com>
2012-02-22 16:05:32 +0200

    Removed superfluous code

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var cheerio = exports = module.exports = require('./lib/cheerio');
-
-/*
-  Attach objects to cheerio
-*/
-exports.parse = cheerio.parse;
-exports.render = cheerio.render;
+exports = module.exports = require('./lib/cheerio');
 
 /*
   Export the version
 */
+
 var version = function() {
   var pkg = require('fs').readFileSync(__dirname + '/package.json', 'utf8');
   return JSON.parse(pkg).version;
