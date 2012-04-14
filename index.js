077274d6c9eeb4066da74cd8e250c49c0e7aafa4
Matt Mueller <mattmuelle@gmail.com>
2012-04-13 18:25:08 -0700

    fixed opening of package a bunch of times

--- index.js
+++ index.js
@@ -3,10 +3,4 @@ exports = module.exports = require('./lib/cheerio');
 /*
   Export the version
 */
-
-var version = function() {
-  var pkg = require('fs').readFileSync(__dirname + '/package.json', 'utf8');
-  return JSON.parse(pkg).version;
-};
-
-exports.__defineGetter__('version', version);
+exports.version = require(__dirname + '/package.json').version;
