97eb62bce7e426d3316248247783dbaa4f5cd47c
Matt Mueller <mattmuelle@gmail.com>
2012-04-13 18:40:59 -0700

    fixed build for 0.4.x

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var fs = require('fs');
+
 exports = module.exports = require('./lib/cheerio');
 
 /*
   Export the version
 */
-exports.version = require(__dirname + '/package.json').version;
+exports.version = (function() {
+  var pkg = fs.readFileSync(__dirname + '/package.json', 'utf8');
+  return JSON.parse(pkg).version;
+})();
