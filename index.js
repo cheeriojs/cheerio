dfbebe1d74d32b8f780bc700a18f95110ec94abb
Matt Mueller <mattmuelle@gmail.com>
2012-05-29 06:51:29 -0700

    compatible with node 4.x

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ exports = module.exports = require('./lib/cheerio');
 /*
   Export the version
 */
-exports.version = require('./package.json').version;
+exports.version = (function() {
+  var pkg = fs.readFileSync(__dirname + '/package.json', 'utf8');
+  return JSON.parse(pkg).version;
+})();
