6da6abcd51451938ae20a93a27037eb0664cd443
Matthew Mueller <mattym@yahoo-inc.com>
2012-07-24 09:13:48 -0700

    fixed default encode/decode levels based on @FB55's comment on #71

--- test/api.attributes.js
+++ test/api.attributes.js
@@ -46,7 +46,7 @@ describe('$(...)', function() {
     it('(key, value) : should correctly encode then decode unsafe values', function() {
       var $apple = $('.apple', fruits);
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
-      expect($apple.get(0).attribs.href).to.equal('http&colon;&sol;&sol;github&period;com&sol;&quot;&gt;&lt;script&gt;alert&lpar;&quot;XSS&excl;&quot;&rpar;&lt;&sol;script&gt;&lt;br');
+      expect($apple.get(0).attribs.href).to.equal('http://github.com/&quot;&gt;&lt;script&gt;alert(&quot;XSS!&quot;)&lt;/script&gt;&lt;br');
       expect($apple.attr('href')).to.equal('http://github.com/"><script>alert("XSS!")</script><br');
 
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
--- test/api.manipulation.js
+++ test/api.manipulation.js
@@ -284,7 +284,7 @@ describe('$(...)', function() {
       var $apple = $('.apple', fruits);
       
       $apple.text('blah <script>alert("XSS!")</script> blah');
-      expect($apple.get(0).children[0].data).to.equal('blah &lt;script&gt;alert&lpar;&quot;XSS&excl;&quot;&rpar;&lt;&sol;script&gt; blah');
+      expect($apple.get(0).children[0].data).to.equal('blah &lt;script&gt;alert(&quot;XSS!&quot;)&lt;/script&gt; blah');
       expect($apple.text()).to.equal('blah <script>alert("XSS!")</script> blah');
 
       $apple.text('blah <script>alert("XSS!")</script> blah');
