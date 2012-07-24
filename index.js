237518aa4f304dc697719c29c28001aad8efd69c
Matthew Mueller <mattym@yahoo-inc.com>
2012-07-24 09:09:31 -0700

    removed node 4.x support

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ exports = module.exports = require('./lib/cheerio');
 /*
   Export the version
 */
-exports.version = (function() {
-  var pkg = fs.readFileSync(__dirname + '/package.json', 'utf8');
-  return JSON.parse(pkg).version;
-})();
+exports.version = require('./package').version;
