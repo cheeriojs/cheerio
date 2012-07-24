cfd9abd0daf5f519f8622e056c338b5eecfd70a9
Matthew Mueller <mattym@yahoo-inc.com>
2012-07-24 09:03:38 -0700

    correctly encoded values

--- test/api.attributes.js
+++ test/api.attributes.js
@@ -46,12 +46,9 @@ describe('$(...)', function() {
     it('(key, value) : should correctly encode then decode unsafe values', function() {
       var $apple = $('.apple', fruits);
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
-      expect($apple.get(0).attribs.href).to.equal('http://github.com/&quot;&gt;&lt;script&gt;alert(&quot;XSS!&quot;)&lt;/script&gt;&lt;br');
+      expect($apple.get(0).attribs.href).to.equal('http&colon;&sol;&sol;github&period;com&sol;&quot;&gt;&lt;script&gt;alert&lpar;&quot;XSS&excl;&quot;&rpar;&lt;&sol;script&gt;&lt;br');
       expect($apple.attr('href')).to.equal('http://github.com/"><script>alert("XSS!")</script><br');
-    });
 
-    it('(key, value) : should correctly encode unsafe values into html', function() {
-      var $apple = $('.apple', fruits);
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
       expect($apple.html()).to.not.contain('<script>alert("XSS!")</script>');
     });
--- test/api.manipulation.js
+++ test/api.manipulation.js
@@ -282,17 +282,14 @@ describe('$(...)', function() {
 
     it('(str) should encode then decode unsafe characters', function() {
       var $apple = $('.apple', fruits);
+      
       $apple.text('blah <script>alert("XSS!")</script> blah');
       expect($apple.get(0).children[0].data).to.equal('blah &lt;script&gt;alert&lpar;&quot;XSS&excl;&quot;&rpar;&lt;&sol;script&gt; blah');
       expect($apple.text()).to.equal('blah <script>alert("XSS!")</script> blah');
-    });
 
-    it('(str) should encode then decode unsafe characters', function() {
-      var $apple = $('.apple', fruits);
       $apple.text('blah <script>alert("XSS!")</script> blah');
       expect($apple.html()).to.not.contain('<script>alert("XSS!")</script>');
     });
-
   });
 
 });
