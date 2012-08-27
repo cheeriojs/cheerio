d7939d4d211c51a8ce14823e5a6e2a2e9ce5c3a1
Matthew Mueller <mattym@yahoo-inc.com>
2012-08-27 14:06:42 -0700

    all is working except root

--- test/api.utils.js
+++ test/api.utils.js
@@ -8,15 +8,14 @@ describe('$', function() {
 
     it('() : should return innerHTML; $.html(obj) should return outerHTML', function() {
       var $div = $('div', '<div><span>foo</span><span>bar</span></div>');
-      var span = $div.children().get(1);
+      var span = $div.children()[1];
       expect($(span).html()).to.equal('bar');
       expect($.html(span)).to.equal('<span>bar</span>');
     });
 
     it('(<obj>) : should accept an object, an array, or a cheerio object', function() {
       var $span = $('<span>foo</span>');
-      expect($.html($span.get(0))).to.equal('<span>foo</span>');
-      expect($.html($span.get())).to.equal('<span>foo</span>');
+      expect($.html($span[0])).to.equal('<span>foo</span>');
       expect($.html($span)).to.equal('<span>foo</span>');
     });
 
@@ -30,6 +29,37 @@ describe('$', function() {
     });
 
   });
+
+
+
+  describe('.load', function() {
+
+    it('(html) : should retain original root after creating a new node', function() {
+      var $html = $.load('<body><ul id="fruits"></ul></body>');
+      expect($html('body')).to.have.length(1);
+      $html('<script>');
+      expect($html('body')).to.have.length(1);
+    });
+
+    it('(html) : should handle lowercase tag options', function() {
+      var $html = $.load('<BODY><ul id="fruits"></ul></BODY>', { lowerCaseTags : true });
+      expect($html.html()).to.be('<body><ul id="fruits"></ul></body>');
+    });
+
+    it('(html) : should handle the ignore whitepace option', function() {
+      var $html = $.load('<body><a href="http://yahoo.com">Yahoo</a> <a href="http://google.com">Google</a></body>', { ignoreWhitespace : true });
+      expect($html.html()).to.be('<body><a href="http://yahoo.com">Yahoo</a><a href="http://google.com">Google</a></body>');
+    });
+
+    // TODO:
+    // it('(html) : should handle xml tag option', function() {
+    //   var $html = $.load('<body><script>oh hai</script></body>', { xmlMode : true });
+    //   console.log($html('script')[0].type);
+    //   expect($html('script')[0].type).to.be('tag');
+    // });
+
+  });
+
 
   describe('.clone', function() {
 
@@ -53,34 +83,6 @@ describe('$', function() {
       $html.root().append('<div id="test"></div>');
       expect($html.html()).to.equal('<div><span>foo</span><span>bar</span></div><div id="test"></div>');
     });
-
-  });
-
-  describe('.load', function() {
-
-    it('(html) : should retain original root after creating a new node', function() {
-      var $html = $.load('<body><ul id="fruits"></ul></body>');
-      expect($html('body')).to.have.length(1);
-      $html('<script>');
-      expect($html('body')).to.have.length(1);
-    });
-
-    it('(html) : should handle lowercase tag options', function() {
-      var $html = $.load('<BODY><ul id="fruits"></ul></BODY>', { lowerCaseTags : true });
-      expect($html.html()).to.be('<body><ul id="fruits"></ul></body>');
-    });
-
-    it('(html) : should handle the ignore whitepace option', function() {
-      var $html = $.load('<body><a href="http://yahoo.com">Yahoo</a> <a href="http://google.com">Google</a></body>', { ignoreWhitespace : true });
-      expect($html.html()).to.be('<body><a href="http://yahoo.com">Yahoo</a><a href="http://google.com">Google</a></body>');
-    });
-
-    // TODO: 
-    // it('(html) : should handle xml tag option', function() {
-    //   var $html = $.load('<body><script>oh hai</script></body>', { xmlMode : true });
-    //   console.log($html('script')[0].type);
-    //   expect($html('script')[0].type).to.be('tag');
-    // });
 
   });
 
