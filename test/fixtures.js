b3dfcc946a04588c5b35ef513e7f76a3b7e92378
Jarno Leppänen <jarno.leppanen@iki.fi>
2013-07-05 16:26:33 +0300

    Added forms test fixture

--- test/fixtures.js
+++ test/fixtures.js
@@ -27,3 +27,12 @@ exports.inputs = [
 	'<input type="radio" value="off" name="radio" /><input type="radio" name="radio" value="on" checked />',
 	'<select id="multi" multiple><option value="1">1</option><option value="2" selected>2</option><option value="3" selected>3</option><option value="4">4</option></select>'
 ].join('');
+
+exports.forms = [
+  '<form id="simple"><input type="text" name="fruit" value="Apple" /></form>',
+  '<form id="nested"><div><input type="text" name="fruit" value="Apple" /></div><input type="text" name="vegetable" value="Carrot" /></form>',
+  '<form id="disabled"><input type="text" name="fruit" value="Apple" disabled /></form>',
+  '<form id="submit"><input type="text" name="fruit" value="Apple" /><input type="submit" name="submit" value="Submit" /></form>',
+  '<form id="select"><select name="fruit"><option value="Apple">Apple</option><option value="Orange" selected>Orange</option></select></form>',
+  '<form id="unnamed"><input type="text" name="fruit" value="Apple" /><input type="text" value="Carrot" /></form>'
+].join('');
