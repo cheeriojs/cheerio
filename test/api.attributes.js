866cce897ca1f3a60ae69af369c65266dfeaa957
Mike Pennisi <mike@mikepennisi.com>
2013-09-10 10:43:08 -0400

    Correct the implementation `filter` and `is`
    According to the jQuery API documentation on `$.fn.filter` [1] and
    `$.fn.is` [2], these methods accept any of the following argument types:
    
    - selector string
    - function
    - element
    - jQuery object
    
    Extend the `$.fn.filter` method to accept elements and Cheerio
    collections. This implicitly fixes the corresponding bug in `$.fn.is`.
    
    [1]: http://api.jquery.com/filter/
    [2]: http://api.jquery.com/is/

--- test/api.attributes.js
+++ test/api.attributes.js
@@ -295,6 +295,28 @@ describe('$(...)', function() {
       expect($('#vegetables', vegetables).is('div')).to.be(false);
     });
 
+    it('(true selection) : should return true', function() {
+      var $vegetables = $('li', vegetables);
+      expect($vegetables.is($vegetables.eq(1))).to.be(true);
+    });
+
+    it('(false selection) : should return false', function() {
+      var $vegetableList = $(vegetables);
+      var $vegetables = $vegetableList.find('li');
+      expect($vegetables.is($vegetableList)).to.be(false);
+    });
+
+    it('(true element) : should return true', function() {
+      var $vegetables = $('li', vegetables);
+      expect($vegetables.is($vegetables[0])).to.be(true);
+    });
+
+    it('(false element) : should return false', function() {
+      var $vegetableList = $(vegetables);
+      var $vegetables = $vegetableList.find('li');
+      expect($vegetables.is($vegetableList[0])).to.be(false);
+    });
+
     it('(true predicate) : should return true', function() {
       var result = $('li', fruits).is(function() {
         return this.hasClass('pear');
