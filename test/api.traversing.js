5c2513bf9e5d80ae00040bc6fee59bc39d883e79
Mike Pennisi <mike@mikepennisi.com>
2013-09-12 20:41:43 -0400

    Remove extra whitespace

--- test/api.traversing.js
+++ test/api.traversing.js
@@ -66,17 +66,17 @@ describe('$(...)', function() {
     it('should only match immediate children, not ancestors');
 
   });
-  
+
   describe('.contents', function() {
-  
+
     it('() : should get all contents', function() {
       expect($('p', text).contents()).to.have.length(5);
     });
-    
+
     it('() : should include text nodes', function() {
       expect($('p', text).contents().first()[0].type).to.equal('text');
     });
-    
+
   });
 
   describe('.next', function() {
