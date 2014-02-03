dbc1b8df3fafd7ff907bf0b56785675d99621805
kpdecker <kpdecker@gmail.com>
2014-02-02 17:27:53 -0800

    Remove unnecessary encoding step for attrs

--- test/api.attributes.js
+++ test/api.attributes.js
@@ -66,7 +66,6 @@ describe('$(...)', function() {
     it('(key, value) : should correctly encode then decode unsafe values', function() {
       var $apple = $('.apple', fruits);
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
-      expect($apple[0].attribs.href).to.equal('http://github.com/&quot;&gt;&lt;script&gt;alert(&quot;XSS!&quot;)&lt;/script&gt;&lt;br');
       expect($apple.attr('href')).to.equal('http://github.com/"><script>alert("XSS!")</script><br');
 
       $apple.attr('href', 'http://github.com/"><script>alert("XSS!")</script><br');
