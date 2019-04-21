0b787d5fb044a6945fbc26348d643b255350a7cf
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-04-22 01:43:39 +0200

    Add a test for checking whether the options are passed for providing the location info from parse5.

--- test/parse.js
+++ test/parse.js
@@ -1,4 +1,5 @@
 var expect = require('expect.js'),
+    assign = require('lodash/assign'),
     parse = require('../lib/parse'),
     defaultOpts = require('../lib/options').default;
 
@@ -352,6 +353,14 @@ describe('parse', function() {
       expect(childNodes[0].tagName).to.be('pre');
       expect(childNodes[0].childNodes[0].data).to.be('A <- factor(A, levels = c("c","a","b"))\n');
     });
+  });
+
+  it('should pass the options for including the location info to parse5', function() {
+    var root = parse('<p>Hello</p>', assign({}, defaultOpts, { _useHtmlParser2: false, locationInfo: true }), false);
+    var location = root.children[0].__location;
+
+    expect(location).to.be.an('object');
+    expect(location.endOffset).to.be(12);
   });
 
 });
