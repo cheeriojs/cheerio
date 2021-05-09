dddd380ef331761fd9af7125287c12adaf1832ba
Felix Böhm <188768+fb55@users.noreply.github.com>
2021-05-09 16:06:33 +0100

    Improve docs for CheerioAPI

--- src/load.ts
+++ src/load.ts
@@ -15,10 +15,38 @@ type StaticType = typeof staticMethods;
 type LoadType = typeof Load;
 
 /**
- * Wrapper around the `Cheerio` class, making it possible to create a new
- * instance without using `new`.
+ * A querying function, bound to a document created from the provided markup.
+ *
+ * Also provides several helper methods for dealing with the document as a whole.
  */
 export interface CheerioAPI extends StaticType, LoadType {
+  /**
+   * This selector method is the starting point for traversing and manipulating
+   * the document. Like jQuery, it's the primary method for selecting elements
+   * in the document.
+   *
+   * `selector` searches within the `context` scope which searches within the
+   * `root` scope.
+   *
+   * @example
+   *
+   * ```js
+   * $('.apple', '#fruits').text();
+   * //=> Apple
+   *
+   * $('ul .pear').attr('class');
+   * //=> pear
+   *
+   * $('li[class=orange]').html();
+   * //=> Orange
+   * ```
+   *
+   * @param selector - Either a selector to look for within the document, or the
+   *   contents of a new Cheerio instance.
+   * @param context - Either a selector to look for within the root, or the
+   *   contents of the document to query.
+   * @param root - Optional HTML document string.
+   */
   <T extends Node, S extends string>(
     selector?: S | BasicAcceptedElems<T>,
     context?: BasicAcceptedElems<Node> | null,
