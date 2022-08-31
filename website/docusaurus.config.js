54bc2be46d1c30a3596695058fba28175d4454a1
Felix Böhm <188768+fb55@users.noreply.github.com>
2022-08-31 14:51:13 +0100

    Initial docusaurus setup

---
+++ website/docusaurus.config.js
@@ -0,0 +1,156 @@
+// @ts-check
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// eslint-disable-next-line node/no-unpublished-require
+const packageJson = require('../package.json');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: packageJson.name,
+  tagline: packageJson.description,
+  url: packageJson.homepage,
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  organizationName: 'cheeriojs', // Usually your GitHub org/user name.
+  projectName: 'cheerio', // Usually your repo name.
+
+  /*
+   * Even if you don't use internalization, you can use this field to set useful
+   * metadata like html lang. For example, if your site is Chinese, you may want
+   * to replace "en" with "zh-Hans".
+   */
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/cheeriojs/cheerio/tree/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/cheeriojs/cheerio/tree/main/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            to: 'docs/api/',
+            label: 'API',
+            position: 'left',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/cheeriojs/cheerio',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/cheerio',
+              },
+              /*
+               * {
+               *   label: 'Discord',
+               *   href: 'https://discordapp.com/invite/docusaurus',
+               * },
+               * {
+               *   label: 'Twitter',
+               *   href: 'https://twitter.com/docusaurus',
+               * },
+               */
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/cheeriojs/cheerio',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} The Cheerio contributors`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+
+      {
+        // TypeDoc options
+        entryPoints: ['../src/index.ts'],
+        tsconfig: '../tsconfig.json',
+        readme: 'none',
+        excludePrivate: true,
+
+        // Plugin options
+        sidebar: {
+          // Always display the API entry last
+          position: Number.MAX_SAFE_INTEGER,
+        },
+      },
+    ],
+  ],
+};
+
+module.exports = config;
