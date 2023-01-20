c4df963964a9342327b359045ce86a9e6588378f
Felix Böhm <188768+fb55@users.noreply.github.com>
2023-01-20 16:18:15 +0000

    Remove blog link (for now)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
             label: 'API',
             position: 'left',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          // { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/cheeriojs/cheerio',
             label: 'GitHub',
