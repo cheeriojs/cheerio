ab1268c9695598ce4ca1879ab808519b5f945c24
Felix Böhm <188768+fb55@users.noreply.github.com>
2023-02-18 12:20:21 +0000

    Add scarf.sh pixel

---
+++ website/src/theme/Footer/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Footer from '@theme-original/Footer';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <Footer {...props} />
+      <img
+        height="0"
+        width="0"
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://static.scarf.sh/a.png?x-pxid=688c9c4c-fb09-42f9-800b-c19f3e057906"
+      />
+    </>
+  );
+}
